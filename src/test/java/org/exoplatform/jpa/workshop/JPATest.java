d62c45ae67622906471a535310df729ebfa6b14a
NgocTM <ngoctm@exoplatform.com>
2015-11-16 10:43:41 +0700

    Exercise 9: Add Liquidbase configuration and update test case.

--- src/test/java/org/exoplatform/jpa/workshop/JPATest.java
+++ src/test/java/org/exoplatform/jpa/workshop/JPATest.java
@@ -2,14 +2,22 @@ package org.exoplatform.jpa.workshop;
 
 import javax.persistence.EntityManager;
 
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import org.exoplatform.commons.persistence.impl.EntityManagerService;
 import org.exoplatform.container.StandaloneContainer;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Assert;
 
-import org.exoplatform.container.ExoContainerContext;
-import java.net.MalformedURLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Created by ngoctm on 11/13/15.
@@ -19,8 +27,18 @@ public class JPATest {
   protected EntityManager entityManager;
   private StandaloneContainer container;
 
+  private Connection conn;
+  private Liquibase liquibase;
+
   @Before
   public void setup() {
+    // Init Liquibase
+    Class.forName("org.hsqldb.jdbcDriver");
+    conn = DriverManager.getConnection("jdbc:hsqldb:mem:db1", "sa", "");
+    Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(conn));
+    liquibase = new Liquibase("db/changelogs/superhero.db.changelog-1.0.0.xml", new ClassLoaderResourceAccessor(), database);
+    liquibase.update((String) null);
+
     // init container
     try {
       container = StandaloneContainer.getInstance();
@@ -36,11 +54,15 @@ public class JPATest {
   }
 
   @After
-  public void teardown() {
+  public void teardown() throws Exception {
     // close entity manager
     if(entityManagerService != null) {
       entityManagerService.endRequest(container);
     }
     container.stop();
+
+    // Close DB
+    liquibase.rollback(1000, null);
+    conn.close();
   }
 }
